189e6b11a3cc8486e98c56ecf0f4ec4eb5492986
ram <sapps1802@gmail.com>
2023-03-08 14:08:55 +0530

    nanda ganda

---
+++ src/components/Appbar.js
@@ -0,0 +1,236 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import './Dashboard.css';
+
+
+
+
+
+
+const pages = ['Home', 'AboutUs', 'Products','Contact Us'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+export default function Appbar(){
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+  
+     
+       
+            return (
+            <>
+           <div className='main-container'>
+           <img src="main.jpg" style={{width:"100%"}}></img>
+                <AppBar position="absolute" color="transparent"  style={{color:'black'}}>
+               
+                  <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                       
+                <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                          mr: 2,
+                          display: { xs: 'none', md: 'flex' },
+                          fontFamily: 'Merriweather',
+                          fontWeight: 700,
+                          letterSpacing: '.3rem',
+                          backgroundColor: 'white',
+                          textDecoration: 'none',
+                          fontWeight:'bold'
+                        }}
+                      >
+                        
+                      </Typography>
+                       {/* <img style={{marginRight:30,alignItems:'center',display:'flex'}} src="logob.png"/>  */}
+            
+                      <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                          size="large"
+                          aria-label="account of current user"
+                          aria-controls="menu-appbar"
+                          aria-haspopup="true"
+                          onClick={handleOpenNavMenu}
+                          color="black"
+                        >
+                          <MenuIcon />
+                        </IconButton>
+                        <Menu
+                          id="menu-appbar"
+                          anchorEl={anchorElNav}
+                          anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                          }}
+                          keepMounted
+                          transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                          }}
+                          open={Boolean(anchorElNav)}
+                          onClose={handleCloseNavMenu}
+                          sx={{
+                            display: { xs: 'block', md: 'none',fontFamily:'Merriweather' },
+                           
+                          }}
+                        >
+                          {pages.map((page) => (
+                            <MenuItem key={page} onClick={handleCloseNavMenu}>
+                              <Typography textAlign="center" style={{fontWeight:'bold',padding:10}}>{page}</Typography>
+                            </MenuItem>
+                          ))}
+                        </Menu>
+                      </Box>
+                      <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                      <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                          mr: 2,
+                          display: { xs: 'flex', md: 'none' },
+                          flexGrow: 1,
+                          fontFamily: 'Merriweather',
+                          fontWeight: 700,
+                          letterSpacing: '.3rem',
+                          color: 'black',
+                          textDecoration: 'none',
+                          fontWeight:'bold'
+                        }}
+                      >
+                        LOGO
+                      </Typography>
+                      <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                          <Button
+                            key={page}
+                            onClick={handleCloseNavMenu}
+                            sx={{ my: 2, color: 'black', display: 'block' ,fontFamily:'Noto Serif',paddingLeft:5,paddingRight:5}}
+                          >
+                           {page}
+                          </Button>
+                        ))}
+                      </Box>
+                      <Search style={{marginRight:100}}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+                      <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                         
+                          </IconButton>
+                        </Tooltip>
+   
+                        <Menu
+                          sx={{ mt: '45px' }}
+                          id="menu-appbar"
+                          anchorEl={anchorElUser}
+                          anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                          }}
+                          keepMounted
+                          transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                          }}
+                          open={Boolean(anchorElUser)}
+                          onClose={handleCloseUserMenu}
+                        >
+                          {settings.map((setting) => (
+                            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                              <Typography textAlign="center">{setting}</Typography>
+                            </MenuItem>
+                          ))}
+                        </Menu>
+                        
+                      </Box>
+                    </Toolbar>
+                  </Container>
+                </AppBar>
+                </div>
+                </>
+              );
+                          }
+            
