c8bbe5ff2191b71688a626a282f3e5dc7066a3fd
sanidhya004 <80572505+sanidhya004@users.noreply.github.com>
2023-03-12 15:13:09 +0530

    khushi gandu home dekh le

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navbar } from './Navbar'
 
 export const Home = () => {
-    return (
-        <div className="relative w-full " >
-            <Navbar />
-            <div id="home" className="p-50 bg-white relative lg: dark:bg-white h-screen" style={{ display: "flex", justifyContent: 'center', alignItems: "center" }} >
-                <img src="logoo.jpg" width={900} style={{ marginLeft: 220 }} />
-                <div className="relative xl:container m-auto px-6 md:px-12 lg:px-full overflow-hidden"  >
+  const[translate,settranslate]=useState("0")
+  const[opacity,setopacity]=useState("50")
+  const handleBrandName=()=>{
+   
+     if(window.scrollY<50){
+       settranslate("full")
+       setopacity("100")
+      
+     }
+     else{
+      settranslate("0")
+      setopacity("50")
+     }
+  }
+const handleLoad=()=>{
+  alert("hey")
+}
+  useEffect(()=>{
+    settranslate("full")
+    setopacity("100")
+    window.addEventListener("scroll",handleBrandName)
+  
+  },[])
+  return (
+    <div className=" flex justify-center items-center h-screen " style={{backgroundImage:"url('Our mission.png')",backgroundSize:"cover"}} >
+    
+    
+     <div className='w-6/12'>
+
+     </div>
+     <div className='w-6/12 flex capitalize gap-10 font-bold  h-6/12  justify-end relative  z-10 ' style={{fontFamily:'Bebas Neue'}}>
+        
+         <p className={`text-9xl cursor-pointer text-white absolute left-4 -translate-x-${translate} opacity-${opacity} transition-all  delay-500 `}>Kad</p><p className='text-9xl cursor-pointer capitalize text-[#2B5DA2] absolute'>Pharmaceuticals</p>
+         <br/><p className='absolute top-40 right-4 text-white font-sans'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+     </div>
+     
 
 
-                    <div className="lg:flex">
-                        <div className="relative mt-8 md:mt-16 space-y-8 sm:w-10/12 md:w-2/3 lg:ml-0 sm:mx-auto text-center lg:text-left lg:mr-auto lg:w-7/12">
-
-                            <span className="block font-semibold text-gray-500 dark:text-gray-400">
-
-                            </span>
-                            <div className="md:mt-0 lg:absolute -left-0 -top-0 lg:w-6/12">
-                                <div className="relative w-full">
-                                    <div
-                                        aria-hidden="true"
-                                        className="absolute scale-75 md:scale-110 inset-0 m-auto w-full h-full md:w-96 md:h-96 rounded-full rotate-45 "
-                                    />
-                                    {/* <img
-  src="pills.png"
-  className="absolute top-0 left-0 bottom-0 "
-  alt="wath illustration"
-  loading="lazy"
-  width={320}
-  height={280}
-/>
-          */}
-                                </div>
-                            </div>
-
-
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    )
+    </div>
+  )
 }
