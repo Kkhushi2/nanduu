ed43e5cbd1f3ec3566596472f7d77e9b32a611af
Kkhushi2 <121499745+Kkhushi2@users.noreply.github.com>
2023-03-14 10:47:53 +0530

    Add files via upload

---
+++ src/components/Ourmission.js
@@ -0,0 +1,30 @@
+import React from "react";
+;
+
+export default function Ourmission(){
+    return (
+        <div className=" w-6/12 bg-white  h-screen text-black  mt-20 "
+         style={{padding:20,marginTop:50}}>
+            <div className=" flex items-center justify-center"> 
+          <img  className=" h-20 w-30 " src="pill.gif"/></div>
+           <div className="text-lg font-bold text-center" style={{fontSize:28,color:"#004080",display:"flex",flexDirection:"column"}}>Our Mission
+           <div className="text-gray-700 text-md font-normal mb-20" style={{fontSize:20}}>CHEMI S.p.A. is specialized in the
+            development, production and sales of active
+             pharmaceutical ingredients. Due to our
+            continuous investment in technology and
+          ability to bring innovation into our
+          manufacturing processes, we are able to offer
+         APIs in several therapeutic areas: oncology,
+             cardiovascular, central nervous system,
+         gastrointestinal and endocrinology.perations which result
+          in the preparation of an active pharmaceutical ingredient. 
+        Major operations or steps in an API process may include multi-step 
+        chemical synthesis and fermentation, purification, 
+ c      rystallization, drying, milling, packing, labeling, and testing.</div>
+           </div>
+          </div>
+      );
+    }
+    
+
+  
